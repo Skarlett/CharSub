de5e4bacd6c7a2a37015948fb917cd72b06f2860
Skarlett <Skarlett@users.noreply.github.com>
2017-12-23 20:06:08 -0600

    Update Generator.py

--- charsub/Generator.py
+++ charsub/Generator.py
@@ -1,30 +1,5 @@
 from charsub.sub import Substitute
 
-
-def permutate_word(word, ruleSet=None, patterns=list()):
-  '''
-  This permutates the word given with substitute characters; like a bruteforce of all possiblities given within the rule set, for more functionality 
-  on this - please refer to sub.py, Utils.py, patterns.py and debugTools.py
-  
-  :param word: Word to permutated
-   :type: str
-  
-  :param ruleSet: A dictionary of rules, the key would be the character to look for, and the left to change it with.
-                  {'.', 'A1'} This will change every instance of "." into every combination of "A1" subsituted
-                  in the same position
-                  
-   :type: dict | str (filePath) | None (Use Defaults)
-  
-  :param patterns: list of Functions from patterns.py | empty list uses defaults
-    :type: list
-  
-  :return: All variations given from the patterns with no duplicates.
-   :type: list
-  '''
-  return Substitute(ruleSet=ruleSet).deform(word, deformFuncList=patterns)
-
-
-
 def BruteForce(end, chars=str(), start=int()):
       ''' 
       This is a classical bruteforce, it will generate all combinations with given characters (chars)
