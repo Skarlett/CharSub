f0b245ec0221d5f321979c838d4e2f221bd931d0
Skarlett <Skarlett@users.noreply.github.com>
2017-12-23 19:24:36 -0600

    Update Generator.py

--- charsub/Generator.py
+++ charsub/Generator.py
@@ -1,5 +1,3 @@
-import Utils
-
 from charsub.sub import Substitute
 
 
@@ -52,83 +50,5 @@ def BruteForce(end, chars=str(), start=int()):
               _round = [x + i for i in chars for x in _round]
           for x in _round:
               yield x
-
-
-def padBruteForceWithWord(word, end, chars='1234567890', start=0, suffix=True, prefix=False):
-    '''
-    :param word: word to pad Brute force with (eg: '(prefix)password(suffix)'
-      :type: str
-      
-    :param end: when to end bruteforce
-      :type: int
-      
-    :param chars: characters to use in bruteforce 
-      :type: str
-    
-    :param start: where to start on bruteforce
-      :type: int 
-    
-    :param suffix: Append to suffix | one of Suffix or Prefix needs to be true
-      :type: bool
-    
-    :param prefix: append to prefix | one of Suffix or Prefix needs to be true
-      :type: bool
-      
-    :return: Yields padded word with bruteforce data 
-    '''
-    try:
-      assert prefix or suffix
-    except:
-      raise AssertionError('Either prefix or suffix has to be true')
-
-    for x in BruteForce(end, chars, start):
-      if suffix:
-        yield word+x
-      elif prefix:
-        yield x+word
-      elif suffix and prefix:
-        yield x+word+x
-
-
-def WordlistAdditives(self, readFrom, writeTo, l, chars='1234567890', start=0, prefix=True, suffix=True, SuffixAndPrefix=True):
-    with open(readFrom) as reader:
-      with open(writeTo, 'a') as f:
-        for word in reader:
-          if not SuffixAndPrefix:
-            if prefix:
-              for x in self.addChars(word.strip(), l, chars, start):
-                f.write(x+'\n')
-            if suffix:
-              for x in self.addChars(word.strip(), l, chars, start, suffix=True):
-                f.write(x+'\n')
-          else:
-            for x in self.addChars(word.strip(), l, chars, start, prefixAndSuffix=True):
-              f.write(x + '\n')
-
-
-def vowelPopper(word, chars=('a', 'e', 'i', 'o', 'u', 'y')):
-  complete = []
-  for stage in range(len(word)):
-    collector = []
-
-    if stage == 0:
-      for pos, letter in enumerate(word):
-        if letter in chars:
-          collector.append(Utils.remove_char(word, pos))
-
-    else:
-      for word in complete:
-        for pos, letter in enumerate(word):
-
-          if letter in chars:
-            data = Utils.remove_char(word, pos)
-            if not data in collector and not data in complete:
-              collector.append(data)
-
-    for x in collector:
-      if len(x) > 0:
-        complete.append(x)
-
-  return set(complete)
 
 
